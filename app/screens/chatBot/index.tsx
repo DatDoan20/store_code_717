76c8cfe8068dcc65fe8a1259aed6e6b5832c3309
ChiHaoLuong <lulubao1234@gmail.com>
2022-10-18 18:06:51 +0700

    chat bot but no user

--- app/screens/chatBot/index.tsx
+++ app/screens/chatBot/index.tsx
@@ -1,14 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
+import RNKommunicateChat from 'react-native-kommunicate-chat';
 
+const startConversation = () => {
+  let conversationObject = {
+    'appId': "994cbdf20d68dc95ab8488273f7f8c2e",
+    // 'withPreChat': true
+  }
 
-
+  RNKommunicateChat.buildConversation(conversationObject, (response, responseMessage) => {
+    if (response == "Success") {
+      console.log("Conversation Successfully with id:" + responseMessage);
+    }
+  });
+}
 
 const ChatBotScreen = () => {
-  const [messages, setMessages] = useState([]);
+
 
   return (
-    <></>
+    <View style={{ flex: 1 }}>
+      <TouchableOpacity onPress={startConversation} >
+        <Text>Start</Text>
+      </TouchableOpacity>
+    </View>
   )
 }
 
